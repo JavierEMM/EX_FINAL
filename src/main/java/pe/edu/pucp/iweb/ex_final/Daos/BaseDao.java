5a9fb2bfa5c10460b9565c92a2131e65fbdf969a
Javier Marroquin <a20193644@pucp.edu.pe>
2021-12-07 11:35:35 -0500

    Primer Commit

--- src/main/java/pe/edu/pucp/iweb/ex_final/Daos/BaseDao.java
+++ src/main/java/pe/edu/pucp/iweb/ex_final/Daos/BaseDao.java
@@ -13,7 +13,7 @@ public abstract class BaseDao {
         }
         String user = "root";
         String pass = "root";
-        String url = "jdbc:mysql://localhost:3306/hr";
+        String url = "jdbc:mysql://localhost:3306/movies";
         return DriverManager.getConnection(url, user, pass);
     }
 }
