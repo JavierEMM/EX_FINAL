ab731af24dbd3e81a6fa8d4d1b9f5d6e7f07903a
Diego Vargas <a20191027@pucp.edu.pe>
2021-12-07 12:36:52 -0500

    Avance 2

--- src/main/java/pe/edu/pucp/iweb/ex_final/Controllers/MainServlet.java
+++ src/main/java/pe/edu/pucp/iweb/ex_final/Controllers/MainServlet.java
@@ -1,9 +1,14 @@
 package pe.edu.pucp.iweb.ex_final.Controllers;
 
+import pe.edu.pucp.iweb.ex_final.Beans.Empleado;
+import pe.edu.pucp.iweb.ex_final.Beans.Rol;
+import pe.edu.pucp.iweb.ex_final.Daos.EmpleadoDao;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "MainServlet", value = "/MainServlet")
 public class MainServlet extends HttpServlet {
@@ -14,12 +19,27 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String dni = request.getParameter("dni");
+        String dniStr = request.getParameter("dni");
         String contrasena = request.getParameter("contrasena");
 
-        EmpleadoDao emppleadoDao = new EmppleadoDao();
+        EmpleadoDao emppleadoDao = new EmpleadoDao();
+        HttpSession session = request.getSession();
+        Empleado empleado = null;
+        try {
+            empleado = emppleadoDao.obtenerEmpleado(Integer.parseInt(dniStr));
+        } catch (SQLException e) {
+            response.sendRedirect(request.getContextPath() + "/LoginServlet");
+        }
 
+        Rol rol = empleado.getRoles().get(0);
 
+        if(rol.getNombre().equalsIgnoreCase("admin")){
+
+        }else if (rol.getNombre().equalsIgnoreCase("gestor")){
+
+        }else if(rol.getNombre().equalsIgnoreCase("vendedor")){
+
+        }
 
     }
 }
