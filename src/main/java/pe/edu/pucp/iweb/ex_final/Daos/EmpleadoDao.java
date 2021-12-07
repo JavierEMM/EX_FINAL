3a2140e45f6f2fac4226823698d42cca2ca83e1b
Jose Ortiz <a20182162@pucp.edu.pe>
2021-12-07 12:33:24 -0500

    obtenerDaoEmpleado

--- src/main/java/pe/edu/pucp/iweb/ex_final/Daos/EmpleadoDao.java
+++ src/main/java/pe/edu/pucp/iweb/ex_final/Daos/EmpleadoDao.java
@@ -4,10 +4,7 @@ import pe.edu.pucp.iweb.ex_final.Beans.Cine;
 import pe.edu.pucp.iweb.ex_final.Beans.Empleado;
 
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class EmpleadoDao extends BaseDao{
@@ -51,6 +48,44 @@ public class EmpleadoDao extends BaseDao{
             e.printStackTrace();
         }
         return  listaEmpleado;
+    }
+
+    public Empleado obtenerEmpleado(int empleadoId) throws SQLException{
+
+        Empleado empleado = null;
+
+        String sql = "SELECT * FROM empleado e WHERE idempleado = ?;";
+
+        try (Connection conn = this.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);){
+
+            pstmt.setInt(1,empleadoId);
+
+            try(ResultSet rs = pstmt.executeQuery();){
+
+                if(rs.next()){
+
+                    empleado = new Empleado();
+                    empleado.setIdEmpleado(rs.getInt(1));
+                    empleado.setNombre(rs.getString(2));
+                    empleado.setApellido(rs.getString(3));
+                    empleado.setDni(rs.getString(4));
+                    empleado.setSalario(rs.getBigDecimal(5));
+                    empleado.setFechaContrato(rs.getString(6));
+                    empleado.setNombreUsuario(rs.getString(7));
+                    empleado.setEdad(rs.getInt(8));
+                    empleado.setActivo(rs.getBoolean(9));
+
+                    Cine cine = new Cine();
+                    cine.setIdCine(rs.getInt(10));
+                    empleado.setCine(cine);
+
+                    Empleado jefe = new Empleado();
+                    jefe.setIdEmpleado(rs.getInt(11));
+                    empleado.setJefe(jefe);
+                }
+            }
+        }return empleado;
     }
 
 }
